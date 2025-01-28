82780323f4a4732fc02d6bd053cc06f3b1cade0c
omerasaf <balikciomerasaf@gmail.com>
2025-01-28 04:23:15 +0300

    added rest data provider service

---
+++ rest-data-provider-service/src/main/java/com/ratedistribution/rdp/RestDataProviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ratedistribution.rdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDataProviderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDataProviderServiceApplication.class, args);
+	}
+
+}
---
+++ rest-data-provider-service/src/test/java/com/ratedistribution/rdp/RestDataProviderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratedistribution.rdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDataProviderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
